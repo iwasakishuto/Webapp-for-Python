ca0c6fcd32b31505e33eb9fdc52d69150574b011
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2019-04-22 15:33:52 +0900

    原型

--- usr/local/app/main.py
+++ usr/local/app/main.py
@@ -1,23 +1,13 @@
 # coding: utf-8
-from flask import Flask, request, render_template
+from flask import Flask,render_template
 
-model = "MODEL"
-app = Flask(__name__) # 初期化(インスタンスとして生成)
+# 初期化
+app = Flask(__name__)
 
-#=== 通常時('/'にアクセスがきた時)の処理 ===
+# ルートアクセス時の処理
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#=== 画像がアップロードされた時('/send'にアクセスがきた時)の処理 ===
-@app.route('/send', methods=['POST'])
-def send():
-    x = request.files['image']
-    """
-    〜〜色々処理〜〜
-    """
-    prediction = model.predict(x) # prediction は、string型とか。
-    return render_template('index.html', class_name=prediction)
-
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run()
